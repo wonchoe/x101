9155e70c4dae5ab7509b76a6b69c41a1474bcb28
oleksii <oleksisem@gmail.com>
2025-08-23 20:28:08 -0400

    test

--- dev/backend.tf
+++ dev/backend.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "s3" {
-    bucket         = "terraform-state-oleksii-140324736561"
+    bucket         = "terraform-state-oleksii-140324736561-dev"
     key            = "dev/terraform.tfstate"
     region         = "us-east-1"
-    dynamodb_table = "terraform-lock-state"
+    dynamodb_table = "terraform-lock-state-dev"
   }
 }
