8b1bc2473658c22acf349253bedbaa0e1deb7d60
oleksii <oleksisem@gmail.com>
2025-08-23 20:48:03 -0400

    bootstrap-prod

---
+++ bootstrap-prod/main.tf
@@ -0,0 +1,55 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_s3_bucket" "tf_state" {
+  bucket = var.bucket_name
+  # lifecycle {
+  #   prevent_destroy = true
+  # }
+}
+
+resource "aws_s3_bucket_versioning" "enables" {
+  bucket = aws_s3_bucket.tf_state.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "default" {
+  bucket = aws_s3_bucket.tf_state.id
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }
+}
+
+
+resource "aws_s3_bucket_public_access_block" "public_access" {
+  bucket                  = aws_s3_bucket.tf_state.id
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
+
+resource "aws_dynamodb_table" "terraform_locks" {
+  name         = var.dynamoDB_name
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "LockID"
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+}
+
+output "bucket_name" {
+  value = var.bucket_name
+}
+
+output "dynamoDB_name" {
+  value = var.dynamoDB_name
+}
---
+++ bootstrap-prod/terraform.tfvars
@@ -0,0 +1,2 @@
+bucket_name   = "terraform-state-oleksii-140324736561-prod"
+dynamoDB_name = "terraform-lock-state-prod"
